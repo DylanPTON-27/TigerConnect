17da32008ac18bd0d82e667e233cd91cc24fde7b
Dylan Conard <dc4986@princeton.edu>
2026-03-24 18:25:19 -0400

    top bar implemented for prototype

--- src/App.jsx
+++ src/App.jsx
@@ -88,16 +88,7 @@ function App() {
 
   return (
     <>
-      <Header />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>TigerConnect</h1>
+       <Header />
 
       <FullCalendar
         plugins={[dayGridPlugin, iCalendarPlugin]}
