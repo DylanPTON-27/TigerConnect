7d11bbc71648a9f325b1edd163f5a34e5ad8692c
Dylan Conard <dc4986@princeton.edu>
2026-03-24 18:25:19 -0400

    top bar implemented for prototype

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+    return (
+        <div class="header">
+            <div class="top-bar">
+                <button> Status </button>
+                <button> Calendar </button>
+            </div>
+
+            <div class="title_top"> TigerConnect </div>
+
+            <div class="top-bar">
+                <button> Bell </button>
+                <button> Settings </button>
+                <button> Logout </button>
+            </div>
+        </div>
+    )
+}
