b5613fe56fc7b56635efd3e44dd6173aca1ea9ca
MS-Awesome321 <ms9454@princeton.edu>
2026-04-06 14:45:40 -0400

    fixed pagination buttons once and for all

--- frontend/src/Main.svelte
+++ frontend/src/Main.svelte
@@ -1,27 +1,39 @@
-<script>
+<script lang="ts">
+	import { Carousel } from "@skeletonlabs/skeleton-svelte";
 	import Calendar from "./Calendar.svelte";
-	import { Carousel } from '@skeletonlabs/skeleton-svelte';
 </script>
 
-<div>
 <Carousel slideCount={2} slidesPerPage={1} spacing="16px">
-	<Carousel.ItemGroup class='h-[80vh] w-[70vw]'>
+	<Carousel.ItemGroup class="carousel">
 		<Carousel.Item index={0}>
 			<Calendar />
 		</Carousel.Item>
 
-		<Carousel.Item index={1}>
-			Other Page
-		</Carousel.Item>
+		<Carousel.Item index={1}>Other Page</Carousel.Item>
 	</Carousel.ItemGroup>
 	<Carousel.IndicatorGroup>
 		<Carousel.Context>
 			{#snippet children(carousel)}
 				{#each carousel().pageSnapPoints as _, index}
-					<Carousel.Indicator {index} class='h-6 w-6 border-2 ml-0.75 mr-0.75'/>
+					<Carousel.Indicator {index} class="indicators" />
 				{/each}
 			{/snippet}
 		</Carousel.Context>
 	</Carousel.IndicatorGroup>
 </Carousel>
-</div>
+
+<style>
+	@import "tailwindcss";
+	@custom-variant dark (&:where([data-mode=dark], [data-mode=dark] *));
+
+	:global(.carousel) {
+		@apply h-[80vh] w-[70vw];
+		@apply m-auto;
+	}
+
+	:global(.indicators) {
+		@apply h-6 w-6;
+		@apply border-2;
+		@apply ml-0.75 mr-0.75;
+	}
+</style>
