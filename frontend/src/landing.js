65c0c9896c57fde4d75a623fa26e89952027bd73
MS-Awesome321 <ms9454@princeton.edu>
2026-04-07 22:42:01 -0400

    gitignore

--- frontend/src/landing.js
+++
@@ -1,9 +0,0 @@
-import { mount } from 'svelte'
-import './app.css'
-import Landing from './LandingView.svelte'
-
-const landing = mount(Landing, {
-  target: document.getElementById('app'),
-})
-
-export default landing
